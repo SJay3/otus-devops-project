668b9c9bc2837efd1b4489d619b9a438be4e5358
Dmitriy Usachev <diman-110@list.ru>
2019-10-08 17:55:19 +0300

    add firewall rules to basic network

--- infra/terraform/network.tf
+++ infra/terraform/network.tf
@@ -17,3 +17,53 @@ resource "google_compute_network" "network" {
 	description = "Network for devops project"
 	auto_create_subnetworks = false
 }
+
+resource "google_compute_firewall" "default-ssh" {
+  name = "${google_compute_network.network.name}-allow-ssh"
+  network = google_compute_network.network.self_link
+
+  priority = 65534
+
+  allow {
+    protocol = "tcp"
+    ports = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_firewall" "default-icmp" {
+  name = "${google_compute_network.network.name}-allow-icmp"
+  network = google_compute_network.network.self_link
+
+  priority = 65534
+
+  allow {
+    protocol = "icmp"
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_firewall" "default-internal" {
+  name = "${google_compute_network.network.name}-allow-internal"
+  network = google_compute_network.network.self_link
+
+  priority = 65534
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports = ["0-65535"]
+  }
+
+  allow {
+    protocol = "udp"
+    ports = ["0-65535"]
+  }
+
+  source_ranges = ["10.200.0.0/16"]
+}
