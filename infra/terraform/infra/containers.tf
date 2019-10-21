6095a8cda3b105ab16a3d259999e3e50f473bb45
Dmitriy Usachev <diman-110@list.ru>
2019-10-21 10:56:30 +0300

    ## [0.1.5] 2019-10-21 ### Removed - [Ansible] fix variables for docker runner registration in infra. Remove unix socket volume mount because it broke dind service. - [Terraform] Remove sleep in ansible provisioner.
    ### Added
    - [Ansible] Add gitlab-runner configuration option `wait_for_services_timeout`. Needs becouse docker:dind starts slowly and warning is shown.
    - [Terraform] Create firewall rules for port 8000 with terraform
    - [Terraform] Add parametr to run provissioner in terraform or not
    - [Ansible] Add waiting until ssh is reacheble. Add playbook `wait.yml`. Import this playbook into gitlab.yml
    
    ### Fixed
    - [Ansible] fix runner registration. Register runner only once. Add when statement to include register task, that true if installation task is changed.
    - [Ansible] Fix host unreacheble when running playbook in terraform

--- infra/terraform/infra/containers.tf
+++ infra/terraform/infra/containers.tf
@@ -5,7 +5,7 @@ resource "null_resource" "deploy_containers" {
   depends_on = [module.docker]
 
   provisioner "local-exec" {
-    command     = "sleep 20 && ansible-playbook --vault-password-file ${var.ansible_vault_key} playbooks/gitlab.yml"
+    command     = "ansible-playbook --vault-password-file ${var.ansible_vault_key} playbooks/gitlab.yml"
     working_dir = "../../ansible"
   }
 }
