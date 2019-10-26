7a139a892fb661ad7f1a8023c0588b944a900580
Dmitriy Usachev <diman-110@list.ru>
2019-10-26 18:09:17 +0300

    swarm cluster playbook

--- infra/terraform/infra/containers.tf
+++ infra/terraform/infra/containers.tf
@@ -5,6 +5,11 @@ resource "null_resource" "deploy_containers" {
   depends_on = [module.docker]
 
   provisioner "local-exec" {
+    command     = "ansible-playbook --vault-password-file ${var.ansible_vault_key} playbooks/infra-tf-configure.yml"
+    working_dir = "../../ansible"
+  }
+
+    provisioner "local-exec" {
     command     = "ansible-playbook --vault-password-file ${var.ansible_vault_key} playbooks/gitlab.yml"
     working_dir = "../../ansible"
   }
